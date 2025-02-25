5e48e6629f66256666fdc4a30315687e88c1bc11
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-02-25 15:04:57 +0100

    Feature: Setup database module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('')],
+  imports: [
+    // initalize  async the mongoose module with the uri from the config service
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      //   list of all dependencies we need to make this work
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
