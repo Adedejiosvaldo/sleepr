7ff984a5d15377a3af96d9c7823d96a82868518f
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-02-26 22:57:11 +0100

    Feature: Added Repository crud to DV

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -28,7 +28,7 @@ export class ReservationsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.reservationsService.findOne(+id);
+    return this.reservationsService.findOne(id);
   }
 
   @Patch(':id')
@@ -36,11 +36,11 @@ export class ReservationsController {
     @Param('id') id: string,
     @Body() updateReservationDto: UpdateReservationDto,
   ) {
-    return this.reservationsService.update(+id, updateReservationDto);
+    return this.reservationsService.update(id, updateReservationDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.reservationsService.remove(+id);
+    return this.reservationsService.remove(id);
   }
 }
--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -7,22 +7,29 @@ import { ReservationRepository } from './reservation.repository';
 export class ReservationsService {
   constructor(private readonly reservationRepository: ReservationRepository) {}
   create(createReservationDto: CreateReservationDto) {
-    return 'This action adds a new reservation';
+    return this.reservationRepository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123',
+    });
   }
 
   findAll() {
-    return `This action returns all reservations`;
+    return this.reservationRepository.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reservation`;
+  findOne(_id: string) {
+    return this.reservationRepository.findById({ _id });
   }
 
-  update(id: number, updateReservationDto: UpdateReservationDto) {
-    return `This action updates a #${id} reservation`;
+  update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservationRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} reservation`;
+  remove(_id: string) {
+    return this.reservationRepository.findOneAndDelete({ _id });
   }
 }
