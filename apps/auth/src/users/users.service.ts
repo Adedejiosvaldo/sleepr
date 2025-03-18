7497557ff58ce034e15e5777cf94febcab82b5cd
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-03-18 13:56:46 +0100

    Feature : Creating a user

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UsersService {
-  async createUser(createUserDTO: CreateUserDTO) {}
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(createUserDTO: CreateUserDTO) {
+    return this.userRepository.create(createUserDTO);
+  }
 }
