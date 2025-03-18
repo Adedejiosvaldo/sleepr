c729a3926b0c86c3d61b03b28189e473e6abf2f6
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-03-18 13:46:57 +0100

    Feature : Adding user  app

---
+++ apps/auth/src/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ UsersModule],
+  controllers: [AuthController, UsersController],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() createUserDTO: CreateUserDTO) {
+    return this.usersService.createUser(createUserDTO);
+  }
+}
